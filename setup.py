71a95eea148950f61d5f70427c5296f503334f0b
Pavol Babincak <pbabinca@redhat.com>
2014-09-26 18:35:58 +0200

    Release 1.19

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="fedpkg",
-    version="1.18",
+    version="1.19",
     author="Dennis Gilmore",
     author_email="dgilmore@fedoraproject.org",
     description=("Fedora plugin to rpkg to manage "
