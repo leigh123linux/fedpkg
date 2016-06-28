3078afb61b58ce0b545b4c4dfaa56716bfae84ef
Chenxiong Qi <cqi@redhat.com>
2016-06-28 14:22:26 +0800

    Release 1.24
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def bash_completion_dir():
 
 setup(
     name="fedpkg",
-    version="1.23",
+    version="1.24",
     author="Dennis Gilmore",
     author_email="dgilmore@fedoraproject.org",
     description=("Fedora plugin to rpkg to manage "
