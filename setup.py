815bc4618a4f5cb46a737df8d39d2dcc1511387c
Chenxiong Qi <cqi@redhat.com>
2018-03-31 19:41:37 +0800

    Use tox to run tests with multiple Python versions
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+
+import os
+import sys
+
 from setuptools import setup, find_packages
 
 try:
@@ -11,6 +15,23 @@ def bash_completion_dir():
     (sts, output) = getstatusoutput(
         'pkg-config --variable=completionsdir bash-completion')
     return output if not sts and output else '/etc/bash_completion.d'
+
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+requirements = os.path.join(project_dir, 'requirements.txt')
+tests_requirements = os.path.join(project_dir, 'tests-requirements.txt')
+
+with open(requirements, 'r') as f:
+    install_requires = [line.strip() for line in f]
+
+with open(tests_requirements, 'r') as f:
+    tests_require = [line.strip() for line in f]
+
+ver = sys.version_info
+if ver[0] <= 2 and ver[1] < 7:
+    tests_require += [
+        'unittest2'
+    ]
 
 
 setup(
@@ -29,7 +50,8 @@ setup(
                 ('/usr/share/zsh/site-functions', ['conf/zsh-completion/_fedpkg']),
                 ],
 
-    tests_require=['nose', 'mock'],
+    install_requires=install_requires,
+    tests_require=tests_require,
     test_suite='nose.collector',
 
     entry_points={
@@ -37,5 +59,19 @@ setup(
             'fedpkg = fedpkg.__main__:main',
             'fedpkg-stage = fedpkg.__main__:main',
         ],
-    }
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
